e82938bc42b23736f53d16121e3e2f63d42003f8
Zowie Erickson <zowie.erickson@gmail.com>
2022-03-28 12:11:25 -0400

    added html container, populating html with json placeholder data

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 fetch("https://jsonplaceholder.typicode.com/posts/")
     .then(response => response.json())
     .then(data => {
-        console.log(data.slice(0, 5))
+        let postsArr = data.slice(0, 5);
+        let html = "";
+        postsArr.forEach(post => {
+            html += `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            <hr />
+            `
+        }) 
+        document.querySelector("#blog-list").innerHTML = html
     })
