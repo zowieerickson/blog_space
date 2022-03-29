0fd0fafb2723f9a88f7d26e13483ebee4afcff5b
Zowie Erickson <zowie.erickson@gmail.com>
2022-03-28 21:57:46 -0400

    Now able to add new entries to blog

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fetch("https://jsonplaceholder.typicode.com/posts/")
         postsArr.forEach(post => {
             html += `
             <div>
-                <h2>${post.title}</h2>
+                <h3>${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
             </div>
@@ -15,16 +15,35 @@ fetch("https://jsonplaceholder.typicode.com/posts/")
         document.querySelector("#blog-list").innerHTML = html
     })
 
-let postArray = [];
-
-document.querySelector("#post-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const postTitle = document.querySelector("#post-title").value
-    const postBody = document.querySelector("#post-body").value
-    postArray.push({
+    
+    document.querySelector("#post-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        const postTitle = document.querySelector("#post-title").value
+        const postBody = document.querySelector("#post-body").value
+        const postData = {
         title: postTitle,
         body: postBody
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts/', {
+        method: 'POST',
+        body: JSON.stringify(postData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
-    console.log(postArray)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let currentStr = document.querySelector("#blog-list").innerHTML;
+            let tempStr =  `
+            <div>
+                <h3>${data.title}</h3>
+                <p>${data.body}</p>
+                <hr />
+            </div>
+            `
+            document.querySelector("#blog-list").innerHTML = tempStr + currentStr
+        })
     
 })
